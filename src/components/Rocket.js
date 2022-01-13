17bcc2140c44bdcba38cdf55027f4a5771a6cac4
Poprotska <marta1404lviv@gmail.com>
2022-01-13 23:07:40 +0200

    homework1

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,7 +1,10 @@
 export default function Rocket({model}) {
     return (
-        <div>
-            {model.mission_name} -{model.launch_year}
+        <div className="container">
+            <div className="text">
+                <div className="name">{model.mission_name}</div>
+                <div className="year">{model.launch_year}</div>
+            </div>
             <img src={model.links.mission_patch} alt="mission_patch"/>
         </div>
     );
