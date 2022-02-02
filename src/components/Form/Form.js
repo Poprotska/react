97ca971bab48bf94202230630d52e21f02b30ea9
Poprotska <marta1404lviv@gmail.com>
2022-02-02 18:12:59 +0200

    cw

---
+++ src/components/Form/Form.js
@@ -0,0 +1,21 @@
+import css from './Form.module.css';
+import {useDispatch} from "react-redux";
+import {addTodo} from "../../store";
+
+const Form = () => {
+  const dispatch = useDispatch();
+  const submit = (e) => {
+      e.preventDefault()
+      dispatch(addTodo({todo:e.target.todo.value}))
+      e.target.reset()
+  }
+
+    return (
+        <form onSubmit={submit} className={css.Form}>
+              <input type="text" name={'todo'}/>
+              <button>Save</button>
+        </form>
+    );
+};
+
+export {Form};
